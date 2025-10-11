f4a147aab53c8ba76231f34f668bd9ea84bd3609
Ikhtiar Hossain <120584860+ikhtiarhossain@users.noreply.github.com>
2025-10-11 14:17:26 +0200

    q2

--- q2/conversion_system_‎moore.sv
+++ q2/conversion_system_‎moore.sv
@@ -5,8 +5,29 @@ module conversion_system_moore (
     output logic z
 );
 
-	// …
-	// Add your description here
-	// …
+    typedef enum logic {
+        S0, S1
+    } state;
+	
+    state present_state, next_state;
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            present_state <= S0;
+        else
+            present_state <= next_state;
+    end
+
+    always_comb begin
+        case (present_state)
+            S0: next_state = x ? S0 : S1;
+            S1: next_state = x ? S1 : S0;
+            default: next_state = S0;
+        endcase
+    end
+
+    always_comb begin
+        z = (present_state == S0) ? 1 : 0;
+    end
 
 endmodule
